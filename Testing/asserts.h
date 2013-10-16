2dd31898161d14eb3b17908fa3ef9f0e46b81281
Paul Hilbert <me@sicgraph.org>
2013-10-16 10:27:54 +0200

    undef fix in Testing/asserts

--- Testing/asserts.h
+++ Testing/asserts.h
@@ -2,12 +2,15 @@
 #define ASSERTS_H_
 
 #ifdef _MSC_VER
+# undef  BOOST_ASSERT_MSG_OSTREAM
 # define BOOST_ASSERT_MSG_OSTREAM std::cout
 #else
+# undef  BOOST_ASSERT_MSG_OSTREAM
 # define BOOST_ASSERT_MSG_OSTREAM std::cerr
 #endif
 #include <boost/assert.hpp>
 
+#undef  asserts
 #define asserts(expr, msg) (BOOST_ASSERT_MSG((expr), (msg)))
 //#define asserts(expr, msg) ((void)0)
 
