aa56f2d92db897925e355fe3bb614232c97aa386
Paul Hilbert <me@sicgraph.org>
2013-11-03 14:33:08 +0100

    split Profiling into .h/.inl and added doxygen comments

--- Testing/Profiling.h
+++ Testing/Profiling.h
@@ -7,34 +7,90 @@
 
 
 namespace Testing {
-	typedef std::chrono::steady_clock       Clock;
-	typedef std::chrono::time_point<Clock>  TimePoint;
-	typedef Clock::duration                 Duration;
-}
-
-#ifdef PROFILING
-namespace Testing {
-	struct TimeInfo {
-		TimePoint start;
-		Duration  passed;
-		bool      finished;
-	};
-} // Testing
-
-#else // !PROFILING
-namespace Testing { struct TimeInfo {}; }
-#endif // PROFILING
-
-namespace Testing {
 
 class Profiling {
 	public:
+		/** Shared pointer to this class */
+		typedef std::shared_ptr<Profiling>        Ptr;
+		/** Weak pointer to this class */
+		typedef std::weak_ptr<Profiling>          WPtr;
+		/** Shared const pointer to this class */
+		typedef std::shared_ptr<const Profiling>  ConstPtr;
+		/** Weak const pointer to this class */
+		typedef std::weak_ptr<const Profiling>    ConstWPtr;
+
+	protected:
+		typedef std::chrono::steady_clock       Clock;
+		typedef std::chrono::time_point<Clock>  TimePoint;
+		typedef Clock::duration                 Duration;
+		struct TimeInfo {
+			TimePoint start;
+			Duration  passed;
+			bool      finished;
+		};
+
+	public:
+		/** 
+		 *  Constructor 
+		 */
 		Profiling();
 
+		/**
+		 *  Start/restarts new profile with given name.
+		 *
+		 *  This member function creates and starts a new time measurement
+		 *  record with the given name. This name should be unique for the
+		 *  measurement and is used to reference (e.g. when stopping) the
+		 *  profile later. It is also used for printing the profile summary
+		 *  using summarize().
+		 *
+		 *  This function may also restarts previously stopped profiles
+		 *  and any measured time is added to the entire measurement of
+		 *  the profile.
+		 *
+		 *  @param name Unique name of the profile to be started.
+		 */
 		void start(std::string name);
+
+		/**
+		 *  Ends measurement of given profile.
+		 *
+		 *  When called, this member function records the time passed
+		 *  since the last call to start(std::string name).
+		 *
+		 *  @param name Unique name of the profile used for the start call.
+		 */
 		void end(std::string name);
+
+		/**
+		 *  Starts/stops profiles depending on their current status.
+		 *
+		 *  This function is a convenience wrapper. On first call it starts
+		 *  a profile with the given name. On second call it stops it, then
+		 *  restarts it and so on...
+		 *
+		 *  @param name Unique name of profile to start/stop.
+		 */
 		void profile(std::string name);
 
+		/**
+		 *  Print summary of all measured profiles.
+		 *
+		 *  Depending on the measurement needs a suitable unit of measurement
+		 *  should be used for the template parameter. Implemented are:
+		 *
+		 *  - std::chrono::hours
+		 *  - std::chrono::minutes
+		 *  - std::chrono::seconds
+		 *  - std::chrono::milliseconds
+		 *  - std::chrono::microseconds
+		 *  - std::chrono::nanoseconds
+		 *
+		 *  The printed unit names and values are changed accordingly.
+		 *
+		 *  @tparam DurationType Unit of measurement to use for the printed duration.
+		 *  @param logFunction If given, this callback function is used to print the summary, otherwise std::cout is used.
+		 */
 		template <class DurationType>
 		void summarize(std::function<void (std::string)> logFunction = nullptr);
 
@@ -49,124 +105,7 @@ class Profiling {
 		std::map< std::string, TimeInfo >  m_profiles;
 };
 
-
-inline Profiling::Profiling() {
-}
-
-#ifdef PROFILING
-inline void Profiling::start(std::string name) {
-	if (m_profiles.find(name) != m_profiles.end()) {
-		m_profiles[name].start = Clock::now();
-		m_profiles[name].finished = false;
-	} else {
-		TimeInfo t;
-		t.start = Clock::now();
-		t.passed = Duration(0);
-		t.finished = false;
-		m_profiles[name] = t;
-	}
-}
-
-inline void Profiling::end(std::string name) {
-	if (!m_profiles.count(name)) return;
-
-	m_profiles[name].passed += Clock::now() - m_profiles[name].start;
-	m_profiles[name].finished = true;
-}
-
-inline void Profiling::profile(std::string name) {
-	if (m_profiles.count(name) && !m_profiles[name].finished) {
-		end(name);
-	} else {
-		start(name);
-	}
-}
-
-template <class DurationType>
-inline void Profiling::summarize(std::function<void (std::string)> logFunction) {
-	if (!m_profiles.size()) return;
-	std::function<void (std::string)> log = logFunction ? std::move(logFunction) : [&] (std::string msg) { std::cout << msg << "\n"; };
-
-	for (auto it = m_profiles.begin(); it != m_profiles.end(); ++it) {
-		if (!(*it).second.finished) {
-			log("Unfinished profile block: " + (*it).first);
-			end((*it).first);
-		}
-	}
-
-	// print empty lines before summary
-	log("");
-	log("Profiling summary");
-
-	typedef typename DurationType::rep Rep;
-	Rep overall(0);
-	for (auto it = m_profiles.begin(); it != m_profiles.end(); ++it) {
-		Rep duration = std::chrono::duration_cast<DurationType>(it->second.passed).count();
-		overall += duration;
-		log(format<DurationType>(it->first, duration));
-		//double fraction = (double)(duration.total_microseconds()) / profileSpan.get().length().total_microseconds();
-	}
-	log(format<DurationType>("Summed: ", overall));
-	log("Finished summary");
-	m_profiles.clear();
-}
-
-#else
-
-inline void Profiling::start(std::string) {
-}
-
-inline void Profiling::end(std::string) {
-}
-
-inline void Profiling::profile(std::string) {
-}
-
-template <class DurationType>
-inline void Profiling::summarize(std::function<void (std::string)>) {
-}
-
-#endif // PROFILING
-
-
-template <class DurationType>
-inline std::string Profiling::format(std::string name, typename DurationType::rep duration) {
-	char output[255];
-	std::string format = "  %-25s%11d"+unit<DurationType>();//   %13.3f%%";
-	sprintf(output, format.c_str(), name.c_str(), duration); //, fraction*100.0);
-	std::string result(output);
-	return result;
-}
-
-template <>
-inline std::string Profiling::unit<std::chrono::hours>() {
-	return "h";
-}
-
-template <>
-inline std::string Profiling::unit<std::chrono::minutes>() {
-	return "m";
-}
-
-template <>
-inline std::string Profiling::unit<std::chrono::seconds>() {
-	return "s";
-}
-
-template <>
-inline std::string Profiling::unit<std::chrono::milliseconds>() {
-	return "ms";
-}
-
-template <>
-inline std::string Profiling::unit<std::chrono::microseconds>() {
-	return "us";
-}
-
-template <>
-inline std::string Profiling::unit<std::chrono::nanoseconds>() {
-	return "ns";
-}
+#include "Profiling.inl"
 
 } // Testing
 
